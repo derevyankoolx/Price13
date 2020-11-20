d7aa76d65547f023c0e4e7cc09ff9ed9d878c481
Alexander Derevyanko <74137399+derevyankoolx@users.noreply.github.com>
2020-11-20 20:31:22 +0300

    price13

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function (){
+    $('.header_slider-descr').slick({
+        arrows: false,
+        dots: true,
+        dotsClass: "header_dots",
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+});
